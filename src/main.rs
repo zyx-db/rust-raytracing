5187e9b8a15e13b267fe7e2daa6e10898900858f
DhirajB-123 <dhirajbomma@gmail.com>
2023-08-26 19:36:04 -0700

    hello world of ray-tracing

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use log::info;
+
+fn main() {
+    let image_height = 256;
+    let image_width = 256;
+
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    
+    for j in 0..image_height {
+        info!("\rScanlines remaining: {}", (image_height - j));
+        for i in 0..image_width {
+            let r: f64 = i as f64 / (image_width as f64 - 1.0);
+            let g: f64 = j as f64 / (image_height as f64 - 1.0);
+            let b: f64 = 0.0;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+            println!("{ir} {ig} {ib}")
+        }
+    }
+}
