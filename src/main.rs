eae27e00bbe7538aa827a64214548df76486663c
DhirajB-123 <dhirajbomma@gmail.com>
2023-08-28 14:06:39 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use log::info;
 mod structs;
-use structs::{Vec3, Color, Ray};
+use structs::{Color, Ray, Vec3};
 
-fn ray_color(r: Ray) -> Color{
+fn ray_color(r: Ray) -> Color {
     let unit_direction = r.direction().unit_vector();
     let a = 0.5 * (unit_direction.y() + 1.0);
     Color::new(
-            (1.0 - a) + (a * 0.5),
-            (1.0 - a) + (a * 0.7),
-            (1.0 - a) + (a * 1.0),
-        )
+        (1.0 - a) + (a * 0.5),
+        (1.0 - a) + (a * 0.7),
+        (1.0 - a) + (a * 1.0),
+    )
 }
 
 fn main() {
@@ -19,7 +19,9 @@ fn main() {
 
     // calculate image height
     let mut image_height = (image_width as f64 / aspect_ratio) as i32;
-    if image_height < 1{image_height = 1;}
+    if image_height < 1 {
+        image_height = 1;
+    }
 
     // camera
     let focal_length = 1.0;
@@ -36,7 +38,8 @@ fn main() {
     let pixel_delta_v = &viewport_v / image_height as f64;
 
     // calculate the location of upper left pixel
-    let viewport_upper_left = camera_center - Vec3::new(0.0, 0.0, focal_length) - &viewport_u / 2.0 - &viewport_v / 2.0;
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - &viewport_u / 2.0 - &viewport_v / 2.0;
     let pixel_top_left_location = &viewport_upper_left + &((&pixel_delta_u + &pixel_delta_v) * 0.5);
 
     // render
@@ -44,11 +47,12 @@ fn main() {
     println!("P3");
     println!("{image_width} {image_height}");
     println!("255");
-    
+
     for j in 0..image_height {
         info!("\rScanlines remaining: {}", (image_height - j));
         for i in 0..image_width {
-            let pixel_center = &(&pixel_top_left_location + &(pixel_delta_u * i as f64)) + &(pixel_delta_v * j as f64);
+            let pixel_center = &(&pixel_top_left_location + &(pixel_delta_u * i as f64))
+                + &(pixel_delta_v * j as f64);
             let ray_direction = pixel_center - camera_center;
             let r = Ray::new(camera_center, ray_direction);
             let pixel_color = ray_color(r);
--- src/structs.rs
+++ src/structs.rs
@@ -1,8 +1,8 @@
 use std::fmt;
-use std::ops::{Add, Sub, Mul, AddAssign, MulAssign, Div, DivAssign};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub};
 
 #[derive(Copy, Clone)]
-pub struct Color{
+pub struct Color {
     values: [f64; 3],
 }
 
@@ -11,152 +11,151 @@ impl fmt::Display for Color {
         let ir = (255.99 * self.values[0]) as i32;
         let ig = (255.99 * self.values[1]) as i32;
         let ib = (255.99 * self.values[2]) as i32;
-        write!(f, "{} {} {}",ir, ig, ib)
+        write!(f, "{} {} {}", ir, ig, ib)
     }
 }
 
-impl Color{
-    pub fn new(r: f64, g: f64, b: f64) -> Self{
+impl Color {
+    pub fn new(r: f64, g: f64, b: f64) -> Self {
         Color { values: [r, g, b] }
     }
 }
 
 #[derive(Copy, Clone)]
-pub struct Vec3{
+pub struct Vec3 {
     pub values: [f64; 3],
 }
 
-impl Vec3 { 
-    pub fn new(x: f64, y: f64, z: f64) -> Self{
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
         Vec3 { values: [x, y, z] }
     }
 
-    pub fn x(&self) -> f64{
+    pub fn x(&self) -> f64 {
         self.values[0]
     }
-    pub fn y(&self) -> f64{
+    pub fn y(&self) -> f64 {
         self.values[1]
     }
-    pub fn z(&self) -> f64{
+    pub fn z(&self) -> f64 {
         self.values[2]
     }
 
-    pub fn negative(&self) -> Vec3{
-        Vec3 { values: 
-            [-self.x(),
-            -self.y(),
-            -self.z()
-        ]}
+    pub fn negative(&self) -> Vec3 {
+        Vec3 {
+            values: [-self.x(), -self.y(), -self.z()],
+        }
     }
 
-    pub fn unit_vector(&self) -> Vec3{
+    pub fn unit_vector(&self) -> Vec3 {
         self / self.length()
     }
 
-    pub fn length(&self) -> f64{
+    pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
     }
 
-    fn length_squared(&self) -> f64{
+    fn length_squared(&self) -> f64 {
         self.x() * self.x() + self.y() * self.y() + self.z() * self.z()
     }
 }
 
-impl Add for &Vec3{
+impl Add for &Vec3 {
     type Output = Vec3;
 
-    fn add(self, other: &Vec3) -> Vec3{
-        Vec3 {values : 
-            [self.x() + other.x(),
-            self.y() + other.y(),
-            self.z() + other.z()]
+    fn add(self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            values: [
+                self.x() + other.x(),
+                self.y() + other.y(),
+                self.z() + other.z(),
+            ],
         }
     }
 }
 
-impl AddAssign for Vec3{
+impl AddAssign for Vec3 {
     fn add_assign(&mut self, other: Self) {
-        *self = Vec3 {values : 
-            [self.x() + other.x(),
-            self.y() + other.y(),
-            self.z() + other.z()]
+        *self = Vec3 {
+            values: [
+                self.x() + other.x(),
+                self.y() + other.y(),
+                self.z() + other.z(),
+            ],
         }
     }
 }
 
-impl Mul<f64> for Vec3{
+impl Mul<f64> for Vec3 {
     type Output = Vec3;
 
-    fn mul(self, other: f64) -> Vec3{
-        Vec3 { values:
-            [self.x() * other,
-            self.y() * other,
-            self.z() * other]
+    fn mul(self, other: f64) -> Vec3 {
+        Vec3 {
+            values: [self.x() * other, self.y() * other, self.z() * other],
         }
     }
 }
 
-impl MulAssign<f64> for Vec3{
+impl MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, other: f64) {
-        *self = Vec3{values :
-            [self.x() * other,
-            self.y() * other,
-            self.z() * other]
+        *self = Vec3 {
+            values: [self.x() * other, self.y() * other, self.z() * other],
         }
     }
 }
 
-impl Sub for Vec3{
+impl Sub for Vec3 {
     type Output = Vec3;
 
-    fn sub(self, other: Vec3) -> Vec3{
-       Vec3 { values: 
-           [self.x() - other.x(),
-           self.y() - other.y(),
-           self.z() - other.z()]
-        } 
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            values: [
+                self.x() - other.x(),
+                self.y() - other.y(),
+                self.z() - other.z(),
+            ],
+        }
     }
 }
 
-impl Div<f64> for &Vec3{
+impl Div<f64> for &Vec3 {
     type Output = Vec3;
 
     fn div(self, other: f64) -> Self::Output {
-        Vec3{values:
-            [self.x() / other,
-            self.y() / other,
-            self.z() / other]
+        Vec3 {
+            values: [self.x() / other, self.y() / other, self.z() / other],
         }
     }
 }
 
-impl DivAssign<f64> for Vec3{
+impl DivAssign<f64> for Vec3 {
     fn div_assign(&mut self, other: f64) {
-       *self = Vec3{values: 
-           [self.x() / other,
-           self.y() / other,
-           self.z() / other]
-       } 
+        *self = Vec3 {
+            values: [self.x() / other, self.y() / other, self.z() / other],
+        }
     }
 }
 
 #[derive(Copy, Clone)]
-pub struct Ray{
-   origin: Vec3,
-   direction: Vec3,
+pub struct Ray {
+    origin: Vec3,
+    direction: Vec3,
 }
 
-impl Ray{
-    pub fn new(o: Vec3, dir: Vec3) -> Ray{
-        Ray { origin: o, direction: dir }
+impl Ray {
+    pub fn new(o: Vec3, dir: Vec3) -> Ray {
+        Ray {
+            origin: o,
+            direction: dir,
+        }
     }
-    pub fn origin(self) -> Vec3{
+    pub fn origin(self) -> Vec3 {
         self.origin
     }
-    pub fn direction(self) -> Vec3{
+    pub fn direction(self) -> Vec3 {
         self.direction
     }
-    pub fn at(self, t: f64) -> Vec3{
-        return &self.origin + &(self.direction * t)
+    pub fn at(self, t: f64) -> Vec3 {
+        return &self.origin + &(self.direction * t);
     }
 }
